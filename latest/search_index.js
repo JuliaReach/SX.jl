0d9b8907c572df99f141120f5514548ef2809f0a
autodocs <autodocs>
2018-03-27 18:06:43 +0000

    build based on d750fdf

---
+++ latest/search_index.js
@@ -0,0 +1,131 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#SX.jl-1",
+    "page": "Home",
+    "title": "SX.jl",
+    "category": "section",
+    "text": "DocTestFilters = [r\"[0-9\\.]+ seconds \\(.*\\)\"]SX is a Julia package to parse SpaceEx modeling files."
+},
+
+{
+    "location": "index.html#Features-1",
+    "page": "Home",
+    "title": "Features",
+    "category": "section",
+    "text": "Parse SX files into types defined in Julia packages HybridSystems.jl and Systems.jl."
+},
+
+{
+    "location": "index.html#Examples-1",
+    "page": "Home",
+    "title": "Examples",
+    "category": "section",
+    "text": "The folder /examples contains a collection of model files that are available from different sources:SpaceEx webpage, small selection of examples that is part of the VM Server distribution and can be executed directly from the SpaceEx web interface."
+},
+
+{
+    "location": "index.html#Library-Outline-1",
+    "page": "Home",
+    "title": "Library Outline",
+    "category": "section",
+    "text": "Pages = [\n    \"lib/types.md\",\n    \"lib/methods.md\"\n]\nDepth = 2"
+},
+
+{
+    "location": "lib/types.html#",
+    "page": "Types",
+    "title": "Types",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "lib/types.html#Types-1",
+    "page": "Types",
+    "title": "Types",
+    "category": "section",
+    "text": "This section describes systems types implemented in SX.jl.Pages = [\"types.md\"]\nDepth = 3CurrentModule = SX\nDocTestSetup = quote\n    using SX\nend"
+},
+
+{
+    "location": "lib/methods.html#",
+    "page": "Methods",
+    "title": "Methods",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "lib/methods.html#Methods-1",
+    "page": "Methods",
+    "title": "Methods",
+    "category": "section",
+    "text": "This section describes systems methods implemented in SX.jl.Pages = [\"methods.md\"]\nDepth = 3CurrentModule = SX\nDocTestSetup = quote\n    using SX\nend"
+},
+
+{
+    "location": "about.html#",
+    "page": "About",
+    "title": "About",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "about.html#About-1",
+    "page": "About",
+    "title": "About",
+    "category": "section",
+    "text": "This page contains some general information about this project, and recommendations about contributing.Pages = [\"about.md\"]"
+},
+
+{
+    "location": "about.html#Contributing-1",
+    "page": "About",
+    "title": "Contributing",
+    "category": "section",
+    "text": "If you like this package, consider contributing! You can send bug reports (or fix them and send your code), add examples to the documentation or propose new features.Below we detail some of the guidelines that should be followed when contributing to this package."
+},
+
+{
+    "location": "about.html#Branches-1",
+    "page": "About",
+    "title": "Branches",
+    "category": "section",
+    "text": "Each pull request (PR) should be pushed in a new branch with the name of the author followed by a descriptive name, e.g. t/mforets/my_feature. If the branch is associated to a previous discussion in one issue, we use the name of the issue for easier lookup, e.g. t/mforets/7."
+},
+
+{
+    "location": "about.html#Unit-testing-and-continuous-integration-(CI)-1",
+    "page": "About",
+    "title": "Unit testing and continuous integration (CI)",
+    "category": "section",
+    "text": "This project is synchronized with Travis CI, such that each PR gets tested before merging (and the build is automatically triggered after each new commit).For the maintainability of this project, we try to understand and fix the failing doctests if they exist. We develop in Julia v0.6.0, but for experimentation we also build on the nightly branch.To run the unit tests locally, you should do:$ julia --color=yes test/runtests.jl"
+},
+
+{
+    "location": "about.html#Contributing-to-the-documentation-1",
+    "page": "About",
+    "title": "Contributing to the documentation",
+    "category": "section",
+    "text": "This documentation is written in Markdown, and it relies on Documenter.jl to produce the HTML layout. To build the docs, run make.jl:$ julia --color=yes docs/make.jl"
+},
+
+{
+    "location": "about.html#Credits-1",
+    "page": "About",
+    "title": "Credits",
+    "category": "section",
+    "text": "These persons have contributed to SX.jl (in alphabetic order):Marcelo Forets\nNikos Kekatos"
+},
+
+]}
